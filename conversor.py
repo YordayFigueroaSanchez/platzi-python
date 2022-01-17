c29a12327deb69436f3a7085e3ecfbe2bf3882a8
yfsanchez <yfsanchez@gmail.com>
2022-01-16 23:35:35 -0300

    primer codigo

---
+++ conversor.py
@@ -0,0 +1,7 @@
+pesos = input("Cuantos pesos :")
+pesos = float(pesos)
+valor_dolar = 45
+dolares = pesos / valor_dolar
+dolares = round(dolares, 2)
+dolares = str(dolares)
+print("Tienes  : " + dolares)
