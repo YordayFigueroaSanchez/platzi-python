b7d8f343aec1284779b577fd3e3426561fa0afce
yfsanchez <yfsanchez@gmail.com>
2022-01-17 22:20:11 -0300

    antes de usar funciones

--- conversor.py
+++ conversor.py
@@ -1,7 +1,24 @@
-pesos = input("Cuantos pesos :")
-pesos = float(pesos)
-valor_dolar = 45
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes  : " + dolares)
+menu = """
+Conversor de monedas a:
+1 - pesos cubanos
+2 - pesos uru
+"""
+opcion = int(input(menu))
+if opcion == 1:
+    pesos = input("Cuantos pesos :")
+    pesos = float(pesos)
+    valor_dolar = 80
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes  : " + dolares)
+elif opcion == 2:
+    pesos = input("Cuantos pesos :")
+    pesos = float(pesos)
+    valor_dolar = 45
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes  : " + dolares)
+else:
+    print("opcion incorrecta")
