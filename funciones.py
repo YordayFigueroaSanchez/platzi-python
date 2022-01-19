7ab56aefe1c4a1029acc856dd07191916487fe6c
yfsanchez <yfsanchez@gmail.com>
2022-01-19 19:20:11 -0300

    funciones 001

--- funciones.py
+++ funciones.py
@@ -1,6 +1,31 @@
 #funciones
-def imprimir_mensaje():
-    print("mensaje en funcion")
+#def imprimir_mensaje():
+#    print("mensaje en funcion")
 
-imprimir_mensaje()
-imprimir_mensaje()
+#imprimir_mensaje()
+#imprimir_mensaje()
+
+def conversacion(mensaje):
+    print("hola")
+    print(mensaje)
+
+def convertir(tipo_pesos,valor_dolar):
+    pesos = int(input("Cuantos pesos " + tipo_pesos))
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes  : " + dolares)
+
+menu = """
+Conversor de monedas a:
+1 - pesos cubanos
+2 - pesos uru
+"""
+opcion = int(input(menu))
+if opcion == 1:
+    convertir('colombianos',2300)
+elif opcion == 2:
+    convertir('cubanos',81)
+else:
+    conversacion(opcion)
+    print("opcion incorrecta")
