f7dc9efece0db24424b16736064fa2ef6af09ba2
yfsanchez <yfsanchez@gmail.com>
2022-01-19 19:51:09 -0300

    bucle while 001

--- bucles.py
+++ bucles.py
@@ -1,10 +1,12 @@
 #bucles
 def run():
     LIMITE = 100
-    contador = 1
-    while contador < LIMITE:
-        print(contador**2)
+    contador = 0
+    potencia_2 = 2**contador
+    while potencia_2 < LIMITE:
+        print("2 elevado a " + str(contador) + " es " + str(potencia_2))
         contador = contador + 1
+        potencia_2 = 2**contador
 
 
 if __name__ == '__main__':
